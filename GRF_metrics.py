7deb0a9375ca52d935bb999e8824564384cde465
Jan Strehl <jan.david.strehl@googlemail.com>
2023-08-22 09:19:49 +0000

    Mod: Autoformat using black

--- GRF_metrics.py
+++ GRF_metrics.py
@@ -1,23 +1,25 @@
 from typing import Any
-import c3d #c3d library
+import c3d  # c3d library
 import numpy as np
 from numpy import linalg as LA
 
 from itertools import product
 
+
 class GRF_metrics:
     def __init__(self) -> None:
         pass
+
     ### TAKEN FROM metadata.py ###
     def print_metadata(self, reader):
-        print('Header information:\n{}'.format(reader.header))
+        print("Header information:\n{}".format(reader.header))
         for key, g in sorted(reader.group_items()):
-            print('')
+            print("")
             for key, p in sorted(g.param_items()):
                 self.print_param(g, p)
 
     def print_param_value(self, name, value):
-        print(name, '=', value)
+        print(name, "=", value)
 
     def print_param_array(self, name, p, offset_in_elements):
         arr = []
@@ -31,12 +33,11 @@ class GRF_metrics:
             return self.print_param_value(name, p.bytes[start:end])
         else:
             arr = p.int8_array
-        print('{0} = {1}'.format(name, arr.flatten()[start:end]))
+        print("{0} = {1}".format(name, arr.flatten()[start:end]))
 
-
-    def print_param(self,g, p):
+    def print_param(self, g, p):
         name = "{0.name}.{1.name}".format(g, p)
-        print('{0}: {1.total_bytes}B {1.dimensions}'.format(name, p))
+        print("{0}: {1.total_bytes}B {1.dimensions}".format(name, p))
 
         if len(p.dimensions) == 0:
             val = None
@@ -55,63 +56,67 @@ class GRF_metrics:
         if len(p.dimensions) >= 2:
             offset = 0
             for coordinate in product(*map(range, reversed(p.dimensions[1:]))):
-                subscript = ''.join(["[{0}]".format(x) for x in coordinate])
+                subscript = "".join(["[{0}]".format(x) for x in coordinate])
                 self.print_param_array(name + subscript, p, offset)
                 offset += p.dimensions[0]
+
     ### (END) TAKEN FROM metadata.py ###
 
     def import_data(self):
-        is_failed = False   
+        is_failed = False
 
-        with open('Sub1_r26_n3.c3d', 'rb') as handle:
+        with open("Sub1_r26_n3.c3d", "rb") as handle:
             reader = c3d.Reader(handle)
             self.print_metadata(reader)
 
         # safe data in a class object
-        return  not is_failed
-
+        return not is_failed
 
     def get_distance(points, GRF):
         # Calcualtes the vector of the distance between the GRF vector and the ankle joint along the sagittal plane
-        vec = np.array([(1),(2),(3)])
+        vec = np.array([(1), (2), (3)])
         return vec
-    
-    def get_F_int(d,F_GRF):
+
+    def get_F_int(d, F_GRF):
         norm_F_GRF = LA.norm(F_GRF)
-        F_int = (norm_F_GRF * d) / (5) # https://www.facebook.com/kevinakirbydpm/photos/a.554861454611102/3747740051989877/?type=3 / 5cm is assumed acc. to paper
+        F_int = (norm_F_GRF * d) / (
+            5
+        )  # https://www.facebook.com/kevinakirbydpm/photos/a.554861454611102/3747740051989877/?type=3 / 5cm is assumed acc. to paper
         return F_int
 
     def calculate_vec_AtoB(point_A, point_B):
         # calculates the vector between 2 points A to B
-        vec = np.zeros((3, 1)) # 3D vector
+        vec = np.zeros((3, 1))  # 3D vector
         vec[0] = point_B[0] - point_A[0]
         vec[1] = point_B[1] - point_A[1]
         vec[2] = point_B[2] - point_A[2]
         return vec
 
-    def project_vector(x,y):
+    def project_vector(x, y):
         # projects vector x onto vector
-        vec = y * np.dot(x, y) / np.dot(y, y) # https://en.wikipedia.org/wiki/Vector_projection
-        return vec 
-    
-    def real_tibial_load():
+        vec = (
+            y * np.dot(x, y) / np.dot(y, y)
+        )  # https://en.wikipedia.org/wiki/Vector_projection
+        return vec
 
-        #LOOP
+    def real_tibial_load():
+        # LOOP
         # Calulate real tibial load
         points, GRF_vec = get_frame()
         tibia_vec = calculate_vec_AtoB(point_ankle, point_knee)
         d = get_distance(points, GRF)
-        
-        F_int = get_F_int(d,GRF)
-        F_ext = project_vector(GRF_vec,tibia_vec)
 
-        F_tot = F_int + F_ext #acc. to paper
-        #ENDLOOP
+        F_int = get_F_int(d, GRF)
+        F_ext = project_vector(GRF_vec, tibia_vec)
+
+        F_tot = F_int + F_ext  # acc. to paper
+        # ENDLOOP
 
         J = F_tot
         F_max = F_tot
-        return J, F_max 
-    
-#MAIN
+        return J, F_max
+
+
+# MAIN
 GRF = GRF_metrics()
 GRF_metrics.import_data(GRF)
